b3c3e70a525dee0aeba76950237b48bdd5197e48
static <TheStaticVoid@users.noreply.github.com>
2023-09-07 15:11:08 -0400

    fix elytra mixin, add template

--- src/main/java/dev/thestaticvoid/capejs/mixin/ElytraLayerMixin.java
+++ src/main/java/dev/thestaticvoid/capejs/mixin/ElytraLayerMixin.java
@@ -2,7 +2,6 @@ package dev.thestaticvoid.capejs.mixin;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
-import dev.thestaticvoid.capejs.CapeJS;
 import dev.thestaticvoid.capejs.CapeRegistry;
 import net.minecraft.client.model.ElytraModel;
 import net.minecraft.client.model.EntityModel;
@@ -14,18 +13,14 @@ import net.minecraft.client.renderer.entity.RenderLayerParent;
 import net.minecraft.client.renderer.entity.layers.ElytraLayer;
 import net.minecraft.client.renderer.entity.layers.RenderLayer;
 import net.minecraft.client.renderer.texture.OverlayTexture;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.PlayerModelPart;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
 import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Mixin(ElytraLayer.class)
 public abstract class ElytraLayerMixin<T extends LivingEntity, M extends EntityModel<T>>
@@ -37,23 +32,16 @@ public abstract class ElytraLayerMixin<T extends LivingEntity, M extends EntityM
         super(renderLayerParent);
     }
 
-    @ModifyVariable(method = "render(Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;ILnet/minecraft/world/entity/LivingEntity;FFFFFF)V", at = @At("STORE"), ordinal = )
-    private ResourceLocation renderMixin() {
-
+    @Inject(method = "render(Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;ILnet/minecraft/world/entity/LivingEntity;FFFFFF)V",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/client/model/ElytraModel;renderToBuffer(Lcom/mojang/blaze3d/vertex/PoseStack;Lcom/mojang/blaze3d/vertex/VertexConsumer;IIFFFF)V",
+            shift = At.Shift.BEFORE), cancellable = true, locals = LocalCapture.CAPTURE_FAILHARD)
+    private void renderMixin(PoseStack matrixStack, MultiBufferSource buffer, int packedLight, T livingEntity, float limbSwing,
+                             float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch, CallbackInfo ci, ItemStack itemStack) {
+        if (livingEntity instanceof AbstractClientPlayer clientPlayer && clientPlayer.isModelPartShown(PlayerModelPart.CAPE) && CapeRegistry.mapContainsPlayer(clientPlayer)) {
+            VertexConsumer vertexConsumer = ItemRenderer.getArmorFoilBuffer(buffer, RenderType.armorCutoutNoCull(CapeRegistry.getResourceByPlayer(clientPlayer)), false, itemStack.hasFoil());
+            this.elytraModel.renderToBuffer(matrixStack, vertexConsumer, packedLight, OverlayTexture.NO_OVERLAY, 1.0f, 1.0f, 1.0f, 1.0f);
+            matrixStack.popPose();
+            ci.cancel();
+        }
     }
-
-//    @Inject(method = "render(Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;ILnet/minecraft/world/entity/LivingEntity;FFFFFF)V",
-//            at = @At(value = "INVOKE", target = "Lnet/minecraft/client/model/ElytraModel;renderToBuffer(Lcom/mojang/blaze3d/vertex/PoseStack;Lcom/mojang/blaze3d/vertex/VertexConsumer;IIFFFF)V",
-//            shift = At.Shift.BEFORE), locals = LocalCapture.CAPTURE_FAILHARD, cancellable = true)
-//    private void renderMixin(PoseStack matrixStack, MultiBufferSource buffer, int packedLight, T livingEntity, float limbSwing,
-//                             float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch, CallbackInfo ci, ItemStack itemStack) {
-//        if (livingEntity instanceof AbstractClientPlayer clientPlayer) {
-//            if (clientPlayer.isModelPartShown(PlayerModelPart.CAPE) && CapeRegistry.mapContainsPlayer(clientPlayer)) {
-//                VertexConsumer vertexConsumer = ItemRenderer.getArmorFoilBuffer(buffer, RenderType.armorCutoutNoCull(CapeRegistry.getResourceByPlayer(clientPlayer)), false, itemStack.hasFoil());
-//                ((ElytraLayer<T, M>)(Object)this).elytraModel.renderToBuffer(matrixStack, vertexConsumer, packedLight, OverlayTexture.NO_OVERLAY, 1.0f, 1.0f, 1.0f, 1.0f);
-//                matrixStack.popPose();
-//                ci.cancel();
-//            }
-//        }
-//    }
 }
